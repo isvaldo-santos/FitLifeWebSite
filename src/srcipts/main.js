9d7dccc363fa812ec93fbd8e14b76f26404a08ce
isvaldo-santos <isvaldosantos@oulook.com>
2021-12-19 21:37:11 -0300

    FitLife non responsive

--- src/srcipts/main.js
+++ src/srcipts/main.js
@@ -1,3 +1,11 @@
+//header Scroll
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0)
+})
+
+// Swiper
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
