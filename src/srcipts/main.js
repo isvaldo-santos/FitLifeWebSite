dc2227e615432a87bbe4514973a2ff9c8d7a5a52
isvaldo-santos <isvaldosantos@oulook.com>
2021-12-23 11:29:08 -0300

    FitLife => Qual é o seu limite?

--- src/srcipts/main.js
+++ src/srcipts/main.js
@@ -1,9 +1,22 @@
 //header Scroll
 let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0)
 })
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active')
+}
+
 
 // Swiper
 var swiper = new Swiper(".home", {
